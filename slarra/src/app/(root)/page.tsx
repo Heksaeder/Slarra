0bab539ad5dd6455451c9ec6a47d4fddddfcc17b
Céline <celine.kurtaran@gmail.com>
2024-05-20 23:14:39 +0200

    [+] added root page

---
+++ slarra/src/app/(root)/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the home page</p>
+      <a href="/profile">Profile</a>
+      <a href="/games">Games</a>
+    </div>
+  )
+}
+
+export default Home
